2f7c385c431c9111367df46163de32e8e3250191
terje <terje@tjervaag.com>
2009-12-18 21:51:07 -0500

    Moved to Module based structure

--- lib/isbndb.rb
+++ lib/isbndb.rb
@@ -3,53 +3,101 @@ require 'rexml/document'
 require 'cgi'
 include REXML
 
-class ISBNDb
-  attr_reader :key
-  ROOTURL = 'http://isbndb.com/api/'
+module ISBNDb
+  @@key = 'REPLACE_WITH_YOUR_ISBNDB_KEY'
+  @@root_url = 'http://isbndb.com/api/'
 
-  def initialize(key)
-    @key = key
-  end
-
-  def authors(q)
-    url = self.url(ROOTURL + 'authors.xml', {:index1 => 'name', :value1 => q})
-    request = Net::HTTP::Get.new(url.path + '?' + url.query, headers)
-    response = Net::HTTP.new(url.host, url.port).start { |http| http.request(request) }
-    doc = Document.new(response.body)
-
-    XPath.match(doc.root, 'AuthorList/AuthorData').collect do |node|
-      [node.attributes['person_id'], node.text('Name').to_s] 
+  def self.__define_accessors
+    class_variables.each do |v| 
+      sym = v.to_s.delete("@").to_sym
+      unless self.respond_to? sym
+        module_eval <<-EOS, __FILE__, __LINE__
+          def self.#{sym}
+            value = if defined?(#{sym.to_s.upcase})
+              #{sym.to_s.upcase}
+            else
+              @@#{sym}
+            end
+            if value.is_a?(Hash)
+              value = (self.domain.nil? ? nil : value[self.domain]) || value.values.first
+            end
+            value
+          end
+        
+          def self.#{sym}=(obj)
+            @@#{sym} = obj
+          end
+        EOS
+      end
     end
   end
 
-  def method_missing(name, *args)
-    super unless name.to_s =~ /^book(s?)_by_\w+$/
-    books(name.to_s.gsub(/book(s?)_by_/, ''), args.first)
-  end
+  __define_accessors
 
-  def books(key, q)
-    url = self.url(ROOTURL + 'books.xml', {:index1 => key, :value1 => q})
-    request = Net::HTTP::Get.new(url.path + '?' + url.query, headers)
-    response = Net::HTTP.new(url.host, url.port).start { |http| http.request(request) }
-    doc = Document.new(response.body)
+  class DB
+    # Returns a list of Authors matching the provided search string
+    def self.authors(query)
+      doc = Util::fetch_xml(Util::url('authors.xml', {:index1 => 'name', :value1 => query}))
+      XPath.match(doc.root, 'AuthorList/AuthorData').collect do |node|
+        Author.new(node.attributes['person_id'], node.text('Name').to_s)
+      end
+    end
 
-    XPath.match(doc.root, "BookList/BookData").collect do |node|
-      {
-        :title => node.text('Title'),
-        :title_long => node.text('TitleLong'),
-        :author => node.text('AuthorsText')
-      }
+    # Returns a list of books matching the given search query.  The search_key can
+    # be one of: isbn, title, combined, full, book_id, person_id, publisher_id,
+    # subject_id, dewey_decimal, lcc_number
+    def self.books(search_key, query)
+      doc = Util::fetch_xml(Util::url('books.xml', {:index1 => search_key, :value1 => query}))
+      XPath.match(doc.root, "BookList/BookData").collect do |node|
+        Book.new(node.text('Title'), node.text('TitleLong'), node.text('AuthorsText'))
+      end
+    end
+
+    # Match fetching books by a search key.  Can be used to call ISBNDb::DB::books by using
+    # methods that are named after the search key.  Examples: ISBNDb::DB::books_by_isbn,
+    # ISBNDb::DB::books_by_person_id.  This method understands the difference between books
+    # and book and will return only the first match if the singular form is used.
+    def self.method_missing(name, *args)
+      if name.to_s =~ /^book(s?)_by_\w+$/
+        # Fetch books using the provided search key
+        books = self.books(name.to_s.gsub(/book(s?)_by_/, ''), args.first)
+        if name.to_s =~ /^book_/
+          # We only want a single book, so return the first one
+          return books.first
+        else
+          # Return all results
+          return books
+        end
+      else
+        # The method called didn't match anything we recognise.  Pass it on.
+        super
+      end
     end
   end
 
-  def headers
-    {
-      "User-Agent" => "Ruby v1.9.1"
-    }
+  class Util
+    # Returns a URL to call the ISBNDb XML API.  Takes a path, such as authors.xml or
+    # books.xml, and a hash of url parameters.  Documentation of URL parameters can be
+    # found at http://isbndb.com/docs/api/
+    def self.url(path, params)
+      # Add the API key to the URL
+      params[:access_key] = ISBNDb::key
+      URI.parse(ISBNDb::root_url + path + '?' + params.collect { |key, value| key.to_s + '=' + CGI.escape(value) }.join('&'))
+    end
+
+    # Fetch a resource from the ISBNDb API and parse the returned data as an XML document
+    def self.fetch_xml(url)
+      # TODO: Error handling
+      request = Net::HTTP::Get.new(url.path + '?' + url.query, Util::headers)
+      response = Net::HTTP.new(url.host, url.port).start { |http| http.request(request) }
+      Document.new(response.body)
+    end
+
+    def self.headers
+      { "User-Agent" => "Ruby v1.9.1" }
+    end
   end
 
-  def url(url, params)
-    params[:access_key] = @key
-    URI.parse(url + '?' + params.collect { |key, value| key.to_s + '=' + CGI.escape(value) }.join('&'))
-  end
+  class Author < Struct.new(:isbndb_id, :name); end
+  class Book < Struct.new(:title, :title_long, :author); end
 end
--- test/test_isbndb.rb
+++ test/test_isbndb.rb
@@ -1,33 +1,40 @@
 require 'helper'
 
 class TestIsbndb < Test::Unit::TestCase
-  context "A User instance" do
+  context "ISBNDb" do
     APIKEY = 'XXXXXXXX'
 
     setup do
-      @isbndb = ISBNDb.new(APIKEY)
+      #@isbndb = ISBNDb.new(APIKEY)
+      ISBNDb::key = APIKEY
     end
 
     should 'store correct API key' do
-      assert_equal @isbndb.key, APIKEY
+      assert_equal ISBNDb::key, APIKEY
     end
 
     should 'return a list of authors from a search query' do
-      authors = @isbndb.authors('Dawkins, Richard')
+      authors = ISBNDb::DB::authors('Dawkins, Richard')
+      ISBNDb::DB::books_by_author('Dawkins, Richard')
       assert_kind_of authors, Array
       assert_operator authors.length, :>=, 1
+      assert_kind_of authors.first, ISBNDb::Author
     end
 
     should 'return a single book result from an ISBN query' do
-      book = @isbndb.book_by_isbn('9781416594789')
+      book = ISBNDb::DB::book_by_isbn('9781416594789')
       assert_not_nil book
-      assert_operator book.length, :==, 1
+      assert_kind_of book, ISBNDb::Book
     end
 
     should 'return all books by a given author' do
-      authors = @isbndb.authors('Dawkins, Richard')
-      books = @isbndb.books_by_person_id(authors.first.first)
+      books = ISBNDb::DB::books_by_person_id(ISBNDb::DB::authors('Dawkins, Richard').first.first)
       assert_operator books.length, :>=, 1
+    end
+
+    should 'return first book by a given author' do
+      book = ISBNDb::DB::book_by_person_id(ISBNDb::DB::authors('Dawkins, Richard').first.first)
+      assert_kind_of book, ISBNDb::Book
     end
   end
 end
